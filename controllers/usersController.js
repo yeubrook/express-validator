bbeb6b73c5c98d989537101a6a62b133ad9c0859
Brook <yeubrook@umich.edu>
2024-11-20 00:25:36 -0500

    Finished foundation with appropriate structure. Storages, routes, views, and controllers all defined.

---
+++ controllers/usersController.js
@@ -0,0 +1,20 @@
+const usersStorage = require("../storages/usersStorage");
+
+exports.usersListGet = (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+};
+
+exports.usersCreateGet = (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+};
+
+exports.usersCreatePost = (req, res) => {
+  const { firstName, lastName } = req.body;
+  usersStorage.addUser({ firstName, lastName });
+  res.redirect("/");
+};
---
+++ storages/usersStorage.js
@@ -0,0 +1,33 @@
+// storages/usersStorage.js
+// This class lets us simulate interacting with a database.
+class UsersStorage {
+    constructor() {
+      this.storage = {};
+      this.id = 0;
+    }
+  
+    addUser({ firstName, lastName }) {
+      const id = this.id;
+      this.storage[id] = { id, firstName, lastName };
+      this.id++;
+    }
+  
+    getUsers() {
+      return Object.values(this.storage);
+    }
+  
+    getUser(id) {
+      return this.storage[id];
+    }
+  
+    updateUser(id, { firstName, lastName }) {
+      this.storage[id] = { id, firstName, lastName };
+    }
+  
+    deleteUser(id) {
+      delete this.storage[id];
+    }
+  }
+
+  module.exports = new UsersStorage();
+  
